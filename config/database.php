0637fec7d803473daad73f342e8872ea2a41c97a
arya-ayush <arya.ayush15@gmail.com>
2018-07-31 09:11:43 +0530

    cors fixing

--- config/database.php
+++ config/database.php
@@ -50,35 +50,35 @@ return [
 
         // use this while deploying to heroku
 
-        'mysql' => [
-
-            'driver'    => 'mysql',
-            'host'      => $hostname,
-            'database'  => $database,
-            'username'  => $username,
-            'password'  => $password,
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ],
-
-//
-//
 //        'mysql' => [
 //
-//            'driver' => 'mysql',
-//            'host' =>$url,
-//            'port' => env('DB_PORT', '3306'),
-//            'database' => env('DB_DATABASE', 'forge'),
-//            'username' => env('DB_USERNAME', 'forge'),
-//            'password' => env('DB_PASSWORD', ''),
-//            'unix_socket' => env('DB_SOCKET', ''),
-//            'charset' => 'utf8mb4',
-//            'collation' => 'utf8mb4_unicode_ci',
-//            'prefix' => '',
-//            'strict' => true,
-//            'engine' => null,
+//            'driver'    => 'mysql',
+//            'host'      => $hostname,
+//            'database'  => $database,
+//            'username'  => $username,
+//            'password'  => $password,
+//            'charset'   => 'utf8',
+//            'collation' => 'utf8_unicode_ci',
+//            'prefix'    => '',
 //        ],
+
+//
+
+        'mysql' => [
+
+            'driver' => 'mysql',
+            'host' =>$url,
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
--- public/index.php
+++ public/index.php
@@ -6,6 +6,22 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 86400');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    } else {
+        header("Access-Control-Allow-Headers:  Authorization, X-Requested-With, Content-Type, Accept");
+    }
+
+    exit(0);
+}
 
 define('LARAVEL_START', microtime(true));
 
