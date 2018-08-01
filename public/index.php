f09a695f7bffa601b4fa21b36df93136d8fe6e20
Kunwar Ashutosh Singh <kunwarashutoshsingh97@gmail.com>
2018-08-01 14:50:02 +0530

    CORS

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *, *");
 header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 header('Access-Control-Allow-Credentials: true');
