8feed911c0523089b2b5f699940d6fa3db4ecf35
Ayush Arya <arya.ayush15@gmail.com>
2018-08-01 13:21:09 +0530

    Update cors.php

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -15,11 +15,14 @@ class cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
-            ->header('Access-Control-Allow-Credentials',' true');
+       header("Access-Control-Allow-Origin: *");
+       header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
+       header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+       header('Access-Control-Allow-Credentials: true');
+ 
+       if (!$request->isMethod('options')) {
+           return $next($request);
+       }
     }
 
 }
