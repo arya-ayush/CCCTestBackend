fd4f85040392553b7afd0ed40c1c9497f45c91be
Kunwar Ashutosh Singh <kunwarashutoshsingh97@gmail.com>
2018-08-01 14:59:41 +0530

    CORS

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,10 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
 
-    $api->get('/question', 'App\Api\V1\Controllers\SectionController@index')->middleware('cors');
-    $api->post('/question', 'App\Api\V1\Controllers\QuestionController@store')->middleware('cors');
-    $api->post('/section', 'App\Api\V1\Controllers\SectionController@store')->middleware('cors');
-    $api->post('/response', 'App\Api\V1\Controllers\UserResponseController@store')->middleware('cors');
-    $api->get('/users', 'App\Api\V1\Controllers\UserResponseController@index')->middleware('cors');
+    $api->get('/question', 'App\Api\V1\Controllers\SectionController@index');
+    $api->post('/question', 'App\Api\V1\Controllers\QuestionController@store');
+    $api->post('/section', 'App\Api\V1\Controllers\SectionController@store');
+    $api->post('/response', 'App\Api\V1\Controllers\UserResponseController@store');
+    $api->get('/users', 'App\Api\V1\Controllers\UserResponseController@index');
 
 });
