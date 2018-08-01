f2fe60fad2993aabc1ca7b0befbaac6439c95e7c
Kunwar Ashutosh Singh <kunwarashutoshsingh97@gmail.com>
2018-08-01 14:44:35 +0530

    CORS

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
 
-header("Access-Control-Allow-Origin: *"); 
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Max-Age: 1000");
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control,Pragma, Authorization, Accept, Accept-Encoding");
-header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+header('Access-Control-Allow-Credentials: true');
 
 /**
  * Laravel - A PHP Framework For Web Artisans
